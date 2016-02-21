ce58b9795c417eef886d2737904d64e8accf573e
Karin-M. K <kmk@kmkmail.com>
2016-02-21 18:38:27 +0100

    add project Task2

---
+++ tasks/Task2/lesson2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var videogames = new []
+			{
+				new Videogame("Super Mario Galaxy", "Nintendo Wii", 40),
+				new Videogame("Lemmings", "Super Nintendo", 35),
+				new Videogame("Worms","PC",20), 
+				new Videogame("Pokemon","GameBoy", 25), 
+				new Videogame("Halo","Xbox",30),
+				new Videogame("Sonic","Sega MasterSystem")
+			};
+
+			Console.WriteLine ("Games in Library: " + Videogame.GameCount);
+		
+			foreach (var b in videogames)
+			{
+				Console.WriteLine("{0,-40} {1,-40} {2,8:0.00} EUR", b.Title, b.Platform, b.CurrentValue);
+
+			}
+
+			foreach (var b in videogames) {
+				if (b.Platform == "PC") {
+					b.CurrentValue = b.CurrentValue + 5;
+				}
+			}
+				
+			Console.WriteLine ();
+
+			foreach (var b in videogames)
+			{
+				Console.WriteLine("{0,-40} {1,-40} {2,8:0.00} EUR {3,8:0.00}", b.Title, b.Platform, b.CurrentValue,b.getValueFactor());
+
+			}
+
+		}
+	}
+}
---
+++ tasks/Task2/lesson2/Videogame.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Task2
+{
+	public class Videogame
+	{
+		private decimal PurchasePrice;
+
+		/// <summary>
+		/// Creates a new Videogame object.
+		/// </summary>
+		/// <param name="title">Title must not be empty.</param>
+		/// <param name="platform">Platform</param>
+		/// <param name="price">Price in EUR must not be negative.</param>
+		public Videogame(string title, string platform, decimal purchasePrice)
+		{
+			if (string.IsNullOrWhiteSpace(title)) throw new ArgumentException("Title must not be empty.", nameof(title));
+			if (string.IsNullOrWhiteSpace(platform)) throw new ArgumentException("Platform must not be empty.", nameof(platform));
+
+			Title = title;
+			Platform = platform;
+			CurrentValue = purchasePrice;
+			PurchasePrice = purchasePrice;
+			GameCount++;
+		}
+	
+		public Videogame(string title, string platform)
+		{
+			if (string.IsNullOrWhiteSpace(title)) throw new ArgumentException("Title must not be empty.", nameof(title));
+			if (string.IsNullOrWhiteSpace(platform)) throw new ArgumentException("Platform must not be empty.", nameof(platform));
+
+			Title = title;
+			Platform = platform;
+			CurrentValue = 20;
+			PurchasePrice = 20;
+			GameCount++;
+		}
+
+		static public int GameCount { get; private set; }
+
+		/// <summary>
+		/// Gets the game title.
+		/// </summary>
+		public string Title { get; }
+
+		/// <summary>
+		/// Gets the Platform.
+		/// </summary>
+		public string Platform { get; }
+
+		/// <summary>
+		/// Gets or sets the current value of the game.
+		/// </summary>
+		/// <value>The current value of the game.</value>
+		public decimal CurrentValue { get; set; }
+
+
+		/// <summary>
+		/// Calculates the value changes. 
+		/// </summary>
+		/// <value>The get value factor.</value>
+		public decimal getValueFactor() {
+			return CurrentValue/PurchasePrice;
+		}
+
+	}
+}
+
