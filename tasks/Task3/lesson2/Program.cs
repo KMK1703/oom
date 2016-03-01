cb0ff0d0652d0f3dad562148e1aeebc7fdf7cd73
Karin-M. K <kmk@kmkmail.com>
2016-03-01 16:34:39 +0100

    add project Task3

---
+++ tasks/Task3/lesson2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Task2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+            var imediums = new IMedium[]
+            {
+                new Videogame("Stick of Truth", "PS3",40,"South Park PRG","Viedeogame"),
+                new Videogame("Golgen Sun", "GameBoy", 20, "Fantasy RPG","Viedeogame"),
+                new MusikCD("Heroes","Sabaton",40,"Limited Edition Earbook","Musik CD", "Heavy Metal"),
+                new MusikCD("Bigger, Better, Faster, More!","4 non Blondes", 15, "Album 1992", "Musik CD", "Alternative-Rock"),
+            };
+			var videogames = new []
+			{
+				new Videogame("Super Mario Galaxy", "Nintendo Wii", 40, "Jump and Run Game", "Viedeogame"),
+				new Videogame("Lemmings", "Super Nintendo", 35, "Help the Lemmings find their way!", "Viedeogame"),
+				new Videogame("Worms","PC",20, "Worms against other Worms, Strategy Game", "Viedeogame"), 
+				new Videogame("Pokemon","GameBoy", 25, "Catch em all!", "Viedeogame"), 
+				new Videogame("Halo","Xbox",30, "First person shooter", "Viedeogame"),			
+			};
+
+            var musicCDs = new[]
+            {
+                new MusikCD("Live Licks","The Rolling Stones", 20, "Live Album 2004", "Musik CD", "Classic Rock"),
+                new MusikCD("Drunken Lullabies","Flogging Molly",15, "Studio Album 2002", "Musik CD", "Irish Folk"),
+                new MusikCD("Live After Death","Iron Maiden",17,"Live Album 1985", "Musik CD","Heavy Metal"),
+                new MusikCD("Back in Black","AC/DC",15,"Studio Album 1980","Musik CD","Hard Rock"),
+            };
+
+            Console.WriteLine();
+            foreach (var b in imediums)
+            {
+                Console.WriteLine("{0,-20}{1,-40}{2,8:0.00} EUR", b.MediumType, b.Title, b.CurrentValue);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine ("Games in Library: " + Videogame.GameCount);
+            Console.WriteLine();
+
+            foreach (var b in videogames)
+			{
+				Console.WriteLine("{0,-40} {1,-40} {2,8:0.00} EUR", b.Title, b.Platform, b.CurrentValue);
+
+			}
+
+            Console.WriteLine();
+
+            foreach (var b in musicCDs)
+            {
+                Console.WriteLine("{0,-20} {1,-20} {2, -20} {3,-20} {4,8:0.00} EUR", b.Title, b.Interpret, b.Description, b.Genre, b.CurrentValue);
+            }
+
+			foreach (var b in videogames) {
+				if (b.Platform == "PC") {
+					b.CurrentValue = b.CurrentValue + 5;
+				}
+			}
+				
+			Console.WriteLine ();
+
+			foreach (var b in videogames)
+			{
+				Console.WriteLine("{0,-40} {1,-40} {2,8:0.00} EUR {3,8:0.00}", b.Title, b.Platform, b.CurrentValue,b.getValueFactor());
+
+			}
+
+		}
+	}
+}
---
+++ tasks/Task3/lesson2/Videogame.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace Task2
+{
+
+    public interface IMedium
+    {
+        /// <summary>
+        /// Gets a textual description of this item.
+        /// </summary>
+        string Description { get; }
+
+        ///<Medium>
+        /// What type of Medium is this?
+        /// </Medium>
+        string MediumType { get; }
+
+        ///<Name>
+        /// Name or Titel of the Medium
+        /// </Name>
+        string Title { get; }
+
+        /// <summary>
+        /// Gets or sets the current value of the game.
+        /// </summary>
+        /// <value>The current value of the game.</value>
+        decimal CurrentValue { get; set; }
+
+    }
+    public class Videogame : IMedium
+    {
+        private decimal PurchasePrice;
+
+        /// <summary>
+        /// Creates a new Videogame object.
+        /// </summary>
+        /// <param name="title">Title must not be empty.</param>
+        /// <param name="platform">Platform</param>
+        /// <param name="price">Price in EUR must not be negative.</param>
+        /// <param name="description">Description</param>
+        /// <param name="mediumtype">Which Medium is this?</param>
+        public Videogame(string title, string platform, decimal purchasePrice, string description, string mediumtype)
+        {
+            if (string.IsNullOrWhiteSpace(title)) throw new ArgumentException("Title must not be empty.", nameof(title));
+            if (string.IsNullOrWhiteSpace(platform)) throw new ArgumentException("Platform must not be empty.", nameof(platform));
+
+            Title = title;
+            Platform = platform;
+            CurrentValue = purchasePrice;
+            PurchasePrice = purchasePrice;
+            description = Description;
+            MediumType = mediumtype;
+            GameCount++;
+        }
+
+
+        static public int GameCount { get; private set; }
+
+        /// <summary>
+        /// Gets the game title.
+        /// </summary>
+        public string Title { get; }
+
+        /// <summary>
+        /// Gets the Platform.
+        /// </summary>
+        public string Platform { get; }
+
+        /// <summary>
+        /// Gets a textual description of this item.
+        /// </summary>
+        public string Description { get; }
+
+        ///<Medium>
+        /// What type of Medium is this?
+        /// </Medium>
+        public string MediumType { get; }
+
+        /// <summary>
+        /// Gets or sets the current value of the game.
+        /// </summary>
+        /// <value>The current value of the game.</value>
+        public decimal CurrentValue { get; set; }
+
+
+        /// <summary>
+        /// Calculates the value changes. 
+        /// </summary>
+        /// <value>The get value factor.</value>
+        public decimal getValueFactor()
+        {
+            return CurrentValue / PurchasePrice;
+        }
+
+    }
+
+    public class MusikCD : IMedium
+    {
+        /// <summary>
+        /// Creates a new Videogame object.
+        /// </summary>
+        /// <param name="title">Title must not be empty.</param>
+        /// <param name="interpret">Interpret</param>
+        /// <param name="price">Price in EUR must not be negative.</param>
+        /// <param name="description">Description</param>
+        /// <param name="mediumtype">Which Medium is this?</param>
+        /// <param name="genre">Which Genre is this?</param>
+        public MusikCD(string title, string interpret, decimal purchasePrice, string description, string mediumtype, string genre)
+        {
+            if (string.IsNullOrWhiteSpace(title)) throw new ArgumentException("Title must not be empty.", nameof(title));
+            if (string.IsNullOrWhiteSpace(interpret)) throw new ArgumentException("Platform must not be empty.", nameof(interpret));
+
+            Title = title;
+            Interpret = interpret;
+            CurrentValue = purchasePrice;
+            Description = description;
+            MediumType = mediumtype;
+            Genre = genre;
+          
+        }
+        /// <summary>
+        /// Gets the game title.
+        /// </summary>
+        public string Title { get; }
+
+        /// <summary>
+        /// Gets the Platform.
+        /// </summary>
+        public string Interpret { get; }
+
+        /// <summary>
+        /// Gets a textual description of this item.
+        /// </summary>
+        public string Description { get; }
+
+        ///<Medium>
+        /// What type of Medium is this?
+        /// </Medium>
+        public string MediumType { get; }
+
+        ///<Medium>
+        /// What type of Medium is this?
+        /// </Medium>
+        public string Genre { get; }
+
+        /// <summary>
+        /// Gets or sets the current value of the game.
+        /// </summary>
+        /// <value>The current value of the game.</value>
+        public decimal CurrentValue { get; set; }
+        
+    }
+}
+   
+
